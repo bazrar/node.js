5893ef1d257237da7e3d66c78f68ecfdd3d46d37
rejan bajracharya <rejanbajracharya@bajratechnologies.com>
2021-09-07 23:37:31 +0545

    created controllers for views

---
+++ controllers/viewController.js
@@ -0,0 +1,22 @@
+const Tour = require('../models/tourModel');
+const catchError = require('../utils/catchError'); 
+
+exports.getOverview = catchError(async (req,res) => {
+
+    //1 get tour data from the collection
+    const tours = await Tour.find();
+
+    //2 build template  
+
+    //3 render that template using tour data from 1; 
+    res.status(200).render('overview', {
+      title: 'All tours', 
+      tours
+    });
+});
+
+exports.getTour = (req,res) => {
+    res.status(200).render('tour', {
+      title: 'The Forest Hiker Tour'
+    });
+};
