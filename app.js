733d9a409ff4fa4d8e7455742b9274a376970742
rejan bajracharya <rejanbajracharya@bajratechnologies.com>
2021-09-07 23:39:29 +0545

    bug fixes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const path = require('path')
 const morgan = require('morgan');
 const errorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const viewRouter = require('./routes/viewRoutes'); 
 
 const AppError = require('./utils/appError')
 
 const app = express();
 
-//serving static files
-app.use(express.static(`${__dirname}/public`));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+// 1. GLOBAL MIDDLEWARES
+//serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 //body parser
 app.use(express.json());
 
@@ -25,6 +31,8 @@ const port = 4000;
 
 //  2. ROUTES MIDDLEWARES
 
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use(errorHandler)
